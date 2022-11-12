f4c482115c4b987c3617c51aa230fda6b914ed2e
kehwa <kehwaweng@staff.ipevo.com>
2022-11-12 20:11:09 +0800

    finsh chatroom base on tcp

---
+++ model/User.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"strconv"
+	"time"
+)
+
+type User struct {
+	ID             int
+	Addr           string
+	EnterAt        time.Time
+	MessageChannel chan string
+}
+
+func (u *User) String() string {
+	return u.Addr + ", UID:" + strconv.Itoa(u.ID) + ", Enter At:" +
+		u.EnterAt.Format("2006-01-02 15:04:05+8000")
+}
